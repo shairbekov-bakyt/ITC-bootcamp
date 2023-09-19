c34130a855bf3150275c273510dda894eb5bd26c
Bakyt Shairbekov <c0mrade@Bakyts-MacBook-Pro.local>
2023-09-19 12:44:53 +0600

    add file main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+text = 'Google создаст 10 специальную команду для поиска 15 багов в особо важных приложениях. 23'
+lists = text.split()
+numbers = []
+letters = []
+
+for i in lists:
+    if i.isdigit():
+        numbers.append(i)
+
+    else:
+        letters.append(i)
+
+print(numbers)
+print(letters)
